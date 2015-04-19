f04592680fe93856dceff3e97785a3d777c226b0
Arnold D. Robbins <arnold@skeeve.com>
2015-04-19 17:28:03 +0300

    Make it compile without warnings.

--- hp.c
+++ hp.c
@@ -2,6 +2,8 @@
 
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAXSTACK	100
 #define TRUE		1
--- hp.gram.y
+++ hp.gram.y
@@ -7,6 +7,7 @@
 #include <math.h>
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int i;
 extern int sound();
